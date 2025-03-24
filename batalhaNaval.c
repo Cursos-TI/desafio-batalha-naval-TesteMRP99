a4c55e6921fd5822dd1a07b4d2d3376d9631384f
TesteMRP99 <mateus.paiva99@hotmail.com>
2025-03-24 20:31:25 +0000

    criacao tabuleiro 10x10 e coordenadas dos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+	printf("		Bem-Vindo a Batalha Naval!!\n");
+	printf("\n");
+	
+	printf("Coordenadas do primeiro navio: [D][3]-[E][3]-[F][3]\n");
+	printf("Coordenadas do segundo navio:  [H][7]-[H][8]-[H][9]\n");
+	char tabuleiro[10][10];
+    
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Inicializa o tabuleiro 10x10 com '0' representando água
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+            tabuleiro[i][j] = '0';
 
-    return 0;
+    // atribuindo de maneira manual a posição do primeiro navio
+    tabuleiro[2][3] = '3';
+    tabuleiro[2][4] = '3';
+    tabuleiro[2][5] = '3';
+
+    // atribuindo de maneira manual a posição do segundo navio
+	tabuleiro[6][7] = '3';
+    tabuleiro[7][7] = '3';
+    tabuleiro[8][7] = '3';			
+
+    // saida dos colunas (A a J)
+    printf("    ");
+    for (int j = 0; j < 10; j++) {
+        printf(" %c ", 'A' + j);
+    }
+    printf("\n");
+
+    // saída com as linhas numeradas de 1 a 10
+    for (int i = 0; i < 10; i++) {
+        printf(" %2d  ", i + 1); // Identifica as linhas com números, utilização do %2d para organizar as linhas quando chegar na linha 10
+		    for (int j = 0; j < 10; j++) {
+            	printf("%c  ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 }
