d52c0c08bb919dd6e00bdaa1f4b3deba6b658434
TesteMRP99 <mateus.paiva99@hotmail.com>
2025-03-28 21:34:33 +0000

    colocando dentro do tabuleiro itens cone, cruz e octaedro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,46 +1,67 @@
+#define LINHA 10
+#define COLUNA 10
 #include <stdio.h>
 
 int main() {
     printf("\t\tBem-Vindo a Batalha Naval!!\n\n");
-    
-    printf("Coordenadas do navio na horizontal: [E][3]-[F][3]-[G][3]\n");
-	printf("Coordenadas do navio na vertical:   [H][7]-[H][8]-[H][9]\n");
-	printf("Coordenadas do navio na diagonal principal: [B][2]-[C][3]-[D][4]\n");
-	printf("Coordenadas do navio na diagonal secundaria: [D][7]-[E][6]-[F][5]\n");
-    
-    char tabuleiro[10][10];
-
+	
+	printf("Cada item será identificado com um número, sendo 1 para o Cone, 2 para a Cruz e 3 para o Octaedro\n");
+	printf("\n");
+	char tabuleiro[LINHA][COLUNA];
     // Inicializa o tabuleiro com água ('0')
-    for (int i = 0; i < 10; i++)
-        for (int j = 0; j < 10; j++)
+    for (int i = 0; i < LINHA; i++){
+        for (int j = 0; j < COLUNA; j++){
             tabuleiro[i][j] = '0';
-
-    // Posiciona o navio na horizontal
-    for (int j = 4; j <= 6; j++)
-        tabuleiro[2][j] = '3';
-
-    // Posiciona o navio na vertical
-    for (int i = 6; i <= 8; i++)
-        tabuleiro[i][7] = '3';
-
-    // Posiciona o navio na diagonal principal (\)
-    for (int i = 0; i < 3; i++)
-        tabuleiro[4 + i][5 - i] = '3';
+        }
+   }
    
-    // Posiciona o navio na diagonal secundária (/)
-    for (int i = 0; i < 3; i++)
-        tabuleiro[1 + i][1 + i] = '3';
-
-    // Exibe o tabuleiro
-    printf("    ");
-    for (int j = 0; j < 10; j++)
+      // Criando o cone usando loops aninhados e estruturas de decisão
+    for (int i = 0; i < 3; i++) { // Define a altura do cone
+        for (int j = 3 - i; j <= 3 + i; j++) { // Define a largura do cone
+            if (j >= 0 && j < COLUNA) { // Garante que está dentro dos limites
+                tabuleiro[i][j] = '1';
+            }
+        }
+    }
+    
+    //criando a Cruz
+    
+         int cruzX = 5; // Coordenada da linha central da cruz, evitando que entre em coolisão com os outros itens
+    int cruzY = 2; // Coordenada da coluna central da cruz, evitando que entre em colisão com os outros itens
+    
+    for (int i = -1; i <= 1; i++) {
+        tabuleiro[cruzX + i][cruzY] = '2'; // Linha vertical
+    }
+    for (int j = -2; j <= 2; j++) {
+        tabuleiro[cruzX][cruzY + j] = '2'; // Linha horizontal
+    }
+    
+    // Criando o octaedro na parte inferior
+     int meio = COLUNA / 2;
+    for (int i = LINHA - 3; i < LINHA; i++) { // Linha vertical 
+        tabuleiro[i][meio] = '3';
+   	 }
+    for (int j = meio - 1; j <= meio + 1; j++) { // Linha horizontal 
+        if (j >= 0 && j < COLUNA) { //garante que está dentro dos limites
+            tabuleiro[LINHA - 2][j] = '3';
+        }
+    }
+    
+    
+		   //imprimindo o cabeçalho das colunas 
+	   printf("    ");
+    for (int j = 0; j < 10; j++){
         printf(" %c ", 'A' + j);
+    }
     printf("\n");
-
-    for (int i = 0; i < 10; i++) {
+   
+   //imprimindo o cabeçalho das linhas 
+   for (int i = 0; i < LINHA; i++) {
         printf(" %2d  ", i + 1);
         for (int j = 0; j < 10; j++)
             printf("%c  ", tabuleiro[i][j]);
         printf("\n");
+        
     }
+    return 0;
 }
