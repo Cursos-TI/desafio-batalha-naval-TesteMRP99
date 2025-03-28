aa9611fb711e797facd7ddf4a1abee4e957eaebb
TesteMRP99 <mateus.paiva99@hotmail.com>
2025-03-28 19:20:40 +0000

    adicao de mais dois navios. diagonal principal e secundaria.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,43 +1,46 @@
 #include <stdio.h>
 
 int main() {
+    printf("\t\tBem-Vindo a Batalha Naval!!\n\n");
     
-	printf("		Bem-Vindo a Batalha Naval!!\n");
-	printf("\n");
-	
-	printf("Coordenadas do primeiro navio: [D][3]-[E][3]-[F][3]\n");
-	printf("Coordenadas do segundo navio:  [H][7]-[H][8]-[H][9]\n");
-	char tabuleiro[10][10];
+    printf("Coordenadas do navio na horizontal: [E][3]-[F][3]-[G][3]\n");
+	printf("Coordenadas do navio na vertical:   [H][7]-[H][8]-[H][9]\n");
+	printf("Coordenadas do navio na diagonal principal: [B][2]-[C][3]-[D][4]\n");
+	printf("Coordenadas do navio na diagonal secundaria: [D][7]-[E][6]-[F][5]\n");
     
+    char tabuleiro[10][10];
 
-    // Inicializa o tabuleiro 10x10 com '0' representando água
+    // Inicializa o tabuleiro com água ('0')
     for (int i = 0; i < 10; i++)
         for (int j = 0; j < 10; j++)
             tabuleiro[i][j] = '0';
 
-    // atribuindo de maneira manual a posição do primeiro navio
-    tabuleiro[2][3] = '3';
-    tabuleiro[2][4] = '3';
-    tabuleiro[2][5] = '3';
+    // Posiciona o navio na horizontal
+    for (int j = 4; j <= 6; j++)
+        tabuleiro[2][j] = '3';
 
-    // atribuindo de maneira manual a posição do segundo navio
-	tabuleiro[6][7] = '3';
-    tabuleiro[7][7] = '3';
-    tabuleiro[8][7] = '3';			
+    // Posiciona o navio na vertical
+    for (int i = 6; i <= 8; i++)
+        tabuleiro[i][7] = '3';
 
-    // saida dos colunas (A a J)
+    // Posiciona o navio na diagonal principal (\)
+    for (int i = 0; i < 3; i++)
+        tabuleiro[4 + i][5 - i] = '3';
+   
+    // Posiciona o navio na diagonal secundária (/)
+    for (int i = 0; i < 3; i++)
+        tabuleiro[1 + i][1 + i] = '3';
+
+    // Exibe o tabuleiro
     printf("    ");
-    for (int j = 0; j < 10; j++) {
+    for (int j = 0; j < 10; j++)
         printf(" %c ", 'A' + j);
-    }
     printf("\n");
 
-    // saída com as linhas numeradas de 1 a 10
     for (int i = 0; i < 10; i++) {
-        printf(" %2d  ", i + 1); // Identifica as linhas com números, utilização do %2d para organizar as linhas quando chegar na linha 10
-		    for (int j = 0; j < 10; j++) {
-            	printf("%c  ", tabuleiro[i][j]);
-        }
+        printf(" %2d  ", i + 1);
+        for (int j = 0; j < 10; j++)
+            printf("%c  ", tabuleiro[i][j]);
         printf("\n");
     }
 }
